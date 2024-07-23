9b6319a07e566f1aedceee4caf61cfc9f88f34a8
Alexander Rubin <your@email.here>
2024-07-22 23:28:12 -0500

    defined get and parse methods using usaddress

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -5,20 +5,31 @@ from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.exceptions import ParseError
 
-
 class Home(TemplateView):
     template_name = 'parserator_web/index.html'
-
 
 class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        # TODO: Flesh out this method to parse an address string using the
-        # parse() method and return the parsed components to the frontend.
-        return Response({})
+        address = request.query_params.get('address', None)
+        if not address:
+            raise ParseError("No address provided")
+
+        address_components, address_type = self.parse(address)
+
+        return Response({
+            'input_string': address,
+            'address_components': address_components,
+            'address_type': address_type
+        })
 
     def parse(self, address):
-        # TODO: Implement this method to return the parsed components of a
-        # given address using usaddress: https://github.com/datamade/usaddress
+        try:
+            parsed_address = usaddress.tag(address)
+            address_components = parsed_address[0]
+            address_type = parsed_address[1]
+        except usaddress.RepeatedLabelError as e:
+            raise ParseError("Invalid address")
+
         return address_components, address_type
