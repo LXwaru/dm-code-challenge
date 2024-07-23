0ab0d2907dbdd0d17f05d6a90a4d3dd25388de22
Alexander Rubin <your@email.here>
2024-07-23 00:02:30 -0500

    test running with the correct api endpoint

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,10 @@
-# Define test fixtures here.
+import os
+import sys
+import django
+
+# Add the parent directory to the sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+def pytest_configure():
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'parserator_web.settings'
+    django.setup()
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,42 @@
 import pytest
+from rest_framework.test import APIClient
 
+@pytest.fixture
+def client():
+    return APIClient()
 
 def test_api_parse_succeeds(client):
-    # TODO: Finish this test. Send a request to the API and confirm that the
-    # data comes back in the appropriate format.
+    # Send a request to the API with a valid address
     address_string = '123 main st chicago il'
-    pytest.fail()
+    response = client.get('/api/parse/', {'address': address_string})
 
+    # Check if the response status code is 200 OK
+    assert response.status_code == 200
+
+    # Check if the response contains the expected data
+    data = response.json()
+    assert 'input_string' in data
+    assert data['input_string'] == address_string
+    assert 'address_components' in data
+    assert 'address_type' in data
+
+    # Check some known components (example, update as needed)
+    assert 'AddressNumber' in data['address_components']
+    assert data['address_components']['AddressNumber'] == '123'
+    assert 'PlaceName' in data['address_components']
+    assert data['address_components']['PlaceName'] == 'chicago'
+    assert 'StateName' in data['address_components']
+    assert data['address_components']['StateName'] == 'il'
 
 def test_api_parse_raises_error(client):
-    # TODO: Finish this test. The address_string below will raise a
-    # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
+    # Send a request to the API with an invalid address
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    response = client.get('/api/parse/', {'address': address_string})
+
+    # Check if the response status code is 400 Bad Request
+    assert response.status_code == 400
+
+    # Check if the response contains the error message
+    data = response.json()
+    assert 'detail' in data
+    assert data['detail'] == 'Invalid address' 
